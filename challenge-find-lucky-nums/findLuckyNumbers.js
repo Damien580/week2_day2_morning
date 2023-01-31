de767aede5bf78f051fa956df04129984163ac98
Damien Redd <damiens.redd@gmail.com>
2023-01-31 09:25:48 -0600

    having trouble writing the function

--- challenge-find-lucky-nums/findLuckyNumbers.js
+++ challenge-find-lucky-nums/findLuckyNumbers.js
@@ -1,7 +1,12 @@
 // Write your code below this line.
-let arr = []
-while(arr.length < 4){
-    var rand = Math.floor(Math.random() * 11);
-    if(arr.indexOf(rand) < 0) arr.push(rand);
-}
-console.log(arr);
+function randomGen(arr){
+    while (arr.length <= arr.length -1){
+        let rand = Math.floor(Math.random() * 11)
+            if (arr.indexOf(rand) < 0){
+                arr.push(rand)
+                return rand
+        }
+    }       
+} 
+    
+console.log(randomGen(2))
